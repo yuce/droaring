d3c9f0b20ad5f45ca10db689ab8d6b4bdea64b4d
Yuce Tekol <yucetekol@gmail.com>
2018-04-03 23:03:05 +0300

    Initial commit

---
+++ source/c.d
@@ -0,0 +1,148 @@
+module roaring.c;
+
+import core.stdc.inttypes: uint8_t, uint16_t, int32_t, uint32_t, uint64_t;
+
+extern(C):
+
+struct roaring_array_t {
+    int32_t size;
+    int32_t allocation_size;
+    void **containers;
+    uint16_t *keys;
+    uint8_t *typecodes;
+}
+
+struct roaring_bitmap_t {
+    roaring_array_t high_low_container;
+    bool copy_on_write;
+}
+
+void roaring_bitmap_add(roaring_bitmap_t *r, uint32_t x);
+
+/**
+ * Add value n_args from pointer vals, faster than repeatedly calling
+ * roaring_bitmap_add
+ *
+ */
+void roaring_bitmap_add_many(roaring_bitmap_t *r, size_t n_args, const uint32_t *vals);
+
+/**
+ * Computes the intersection between two bitmaps and returns new bitmap. The
+ * caller is
+ * responsible for memory management.
+ *
+ */
+roaring_bitmap_t *roaring_bitmap_and(const roaring_bitmap_t *x1, const roaring_bitmap_t *x2);
+
+bool roaring_bitmap_contains(const roaring_bitmap_t *r, uint32_t val);
+roaring_bitmap_t *roaring_bitmap_create();
+roaring_bitmap_t *roaring_bitmap_create_with_capacity(uint32_t cap);
+
+/**
+ * read a bitmap from a serialized version. This is meant to be compatible with
+ * the Java and Go versions. See format specification at
+ * https://github.com/RoaringBitmap/RoaringFormatSpec
+ * In case of failure, a null pointer is returned.
+ * This function is unsafe in the sense that if there is no valid serialized
+ * bitmap at the pointer, then many bytes could be read, possibly causing a buffer
+ * overflow. For a safer approach,
+ * call roaring_bitmap_portable_deserialize_safe.
+ */
+roaring_bitmap_t *roaring_bitmap_portable_deserialize(const char *buf);
+
+/**
+ * read a bitmap from a serialized version in a safe manner (reading up to maxbytes).
+ * This is meant to be compatible with
+ * the Java and Go versions. See format specification at
+ * https://github.com/RoaringBitmap/RoaringFormatSpec
+ * In case of failure, a null pointer is returned.
+ */
+roaring_bitmap_t *roaring_bitmap_portable_deserialize_safe(const char *buf, size_t maxbytes);
+
+/**
+ * Check how many bytes would be read (up to maxbytes) at this pointer if there
+ * is a bitmap, returns zero if there is no valid bitmap.
+ * This is meant to be compatible with
+ * the Java and Go versions. See format specification at
+ * https://github.com/RoaringBitmap/RoaringFormatSpec
+ */
+size_t roaring_bitmap_portable_deserialize_size(const char *buf, size_t maxbytes);
+
+/**
+ * Return true if the two bitmaps contain the same elements.
+ */
+bool roaring_bitmap_equals(const roaring_bitmap_t *ra1, const roaring_bitmap_t *ra2);
+
+void roaring_bitmap_free(roaring_bitmap_t *r);
+roaring_bitmap_t *roaring_bitmap_from_range(uint64_t min, uint64_t max, uint32_t step);
+uint64_t roaring_bitmap_get_cardinality(const roaring_bitmap_t *ra);
+uint32_t roaring_bitmap_maximum(const roaring_bitmap_t *bm);
+uint32_t roaring_bitmap_minimum(const roaring_bitmap_t *bm);
+
+/**
+ * Computes the union between two bitmaps and returns new bitmap. The caller is
+ * responsible for memory management.
+ */
+roaring_bitmap_t *roaring_bitmap_or(const roaring_bitmap_t *x1, const roaring_bitmap_t *x2);
+
+void roaring_bitmap_printf(const roaring_bitmap_t *ra);
+void roaring_bitmap_printf_describe(const roaring_bitmap_t *ra);
+
+/**
+* roaring_bitmap_rank returns the number of integers that are smaller or equal
+* to x.
+*/
+uint64_t roaring_bitmap_rank(const roaring_bitmap_t *bm, uint32_t x);
+
+/**
+ * write a bitmap to a char buffer.  The output buffer should refer to at least
+ *  roaring_bitmap_portable_size_in_bytes(ra) bytes of allocated memory.
+ * This is meant to be compatible with
+ * the
+ * Java and Go versions. Returns how many bytes were written which should be
+ * roaring_bitmap_portable_size_in_bytes(ra).  See format specification at
+ * https://github.com/RoaringBitmap/RoaringFormatSpec
+ */
+size_t roaring_bitmap_portable_serialize(const roaring_bitmap_t *ra, char *buf);
+
+/**
+ * How many bytes are required to serialize this bitmap (meant to be compatible
+ * with Java and Go versions).  See format specification at
+ * https://github.com/RoaringBitmap/RoaringFormatSpec
+ */
+size_t roaring_bitmap_portable_size_in_bytes(const roaring_bitmap_t *ra);
+
+void roaring_bitmap_remove(roaring_bitmap_t *r, uint32_t x);
+bool roaring_bitmap_run_optimize(roaring_bitmap_t *r);
+bool roaring_bitmap_select(const roaring_bitmap_t *bm, uint32_t rank, uint32_t *element);
+
+/**
+* write the bitmap to an output pointer, this output buffer should refer to
+* at least roaring_bitmap_size_in_bytes(ra) allocated bytes.
+*
+* see roaring_bitmap_portable_serialize if you want a format that's compatible
+* with Java and Go implementations
+*
+* this format has the benefit of being sometimes more space efficient than
+* roaring_bitmap_portable_serialize
+* e.g., when the data is sparse.
+*
+* Returns how many bytes were written which should be
+* roaring_bitmap_size_in_bytes(ra).
+*/
+size_t roaring_bitmap_serialize(const roaring_bitmap_t *ra, char *buf);
+
+/**
+ * How many bytes are required to serialize this bitmap (NOT compatible
+ * with Java and Go versions)
+ */
+size_t roaring_bitmap_size_in_bytes(const roaring_bitmap_t *ra);
+
+/**
+ * Convert the bitmap to an array. Write the output to "ans",
+ * caller is responsible to ensure that there is enough memory
+ * allocated
+ * (e.g., ans = malloc(roaring_bitmap_get_cardinality(mybitmap)
+ *   * sizeof(uint32_t))
+ */
+void roaring_bitmap_to_uint32_array(const roaring_bitmap_t *ra, uint32_t *ans);
---
+++ source/roaring.d
@@ -0,0 +1,265 @@
+
+module roaring.roaring;
+
+import roaring.c;
+
+class Roaring {
+    /// Creates an empty bitmap
+    this() {
+        this(roaring_bitmap_create());
+    }
+    
+    ~this() {
+        roaring_bitmap_free(this.roaring);
+        this.roaring = null;
+    }
+
+    /** Creates an empty bitmap with a predefined capacity
+        PARAMS
+            uint cap: The predefined capacity
+    */
+    this(uint cap) {
+        this(roaring_bitmap_create_with_capacity(cap));
+    }
+
+    private this(roaring_bitmap_t *r) {
+        this.roaring = r;
+    }
+
+    static Roaring bitmapOf(uint[] args ...) {
+        return Roaring.fromArray(args);
+    }
+
+    static Roaring fromArray(uint[] bits) {
+        Roaring r = new Roaring(cast(uint)bits.length);
+        roaring_bitmap_add_many(r.roaring, bits.length, bits.ptr);
+        return r;
+    }
+
+    static Roaring fromRange(ulong min, ulong max, uint step) {
+        auto rr = roaring_bitmap_from_range(min, max, step);
+        return new Roaring(rr);
+    }
+
+    static Roaring read(const char[] buf) {
+        auto rr = roaring_bitmap_portable_deserialize_safe(buf.ptr, buf.length);
+        if (!rr) {
+            // TODO: raise an exception
+            return null;
+        }
+        return new Roaring(rr);
+    }
+
+    @property
+    ulong cardinality() const {
+        return roaring_bitmap_get_cardinality(this.roaring);
+    }
+    @property
+    void copyOnWrite(bool enable) {
+        this.roaring.copy_on_write = enable;
+    }
+
+    uint[] toArray() const {
+        uint[] a = new uint[this.cardinality];
+        roaring_bitmap_to_uint32_array(this.roaring, a.ptr);
+        return a[];
+    }
+
+    void add(uint x) {
+        roaring_bitmap_add(this.roaring, x);
+    }
+
+    /**
+     * Remove value x
+     *
+     */
+    void remove(uint x) {
+        roaring_bitmap_remove(this.roaring, x);
+    }
+
+    /**
+     * Check if value x is present
+     */
+    bool contains(uint x) const {
+        return roaring_bitmap_contains(this.roaring, x);
+    }
+
+    /**
+     * Return the largest value (if not empty)
+     *
+     */
+    @property
+    uint maximum() const {
+        return roaring_bitmap_maximum(this.roaring);
+    }
+
+    /**
+    * Return the smallest value (if not empty)
+    *
+    */
+    @property
+    uint minimum() const {
+        return roaring_bitmap_minimum(this.roaring);
+    }
+
+    @property
+    size_t sizeInBytes() const {
+        return roaring_bitmap_size_in_bytes(this.roaring);
+    }
+
+    void printf() const {
+        roaring_bitmap_printf(this.roaring);
+    }
+
+    /**
+    * Returns the number of integers that are smaller or equal to x.
+    */
+    ulong rank(uint rank) const {
+        return roaring_bitmap_rank(this.roaring, rank);
+    }
+
+    bool select(uint rank, out uint elem) const {
+        return roaring_bitmap_select(this.roaring, rank, &elem);
+    }
+
+    bool optimize() {
+        return roaring_bitmap_run_optimize(this.roaring);
+    }
+
+    char[] write(const bool portable = true) const
+    {
+        char[] buf;
+        ulong size;
+        if (portable) {
+            buf = new char[roaring_bitmap_portable_size_in_bytes(this.roaring)];
+            size = roaring_bitmap_portable_serialize(this.roaring, buf.ptr);
+        }
+        else {
+            buf = new char[roaring_bitmap_size_in_bytes(this.roaring)];
+            size = roaring_bitmap_serialize(this.roaring, buf.ptr);
+        }
+        return buf[0..size];
+    }
+
+    Roaring opBinary(const string op)(const Roaring b) const {
+        roaring_bitmap_t *result;
+        static if (op == "&") result = roaring_bitmap_and(this.roaring, b.roaring);
+        else static if (op == "|") result = roaring_bitmap_or(this.roaring, b.roaring);
+        else static assert(0, "Operator " ~ op ~ " not implemented.");
+        
+        return new Roaring(result);
+    }
+
+    override bool opEquals(Object b) {
+        import std.stdio : writeln;
+        if (this is b) return true;
+        if (b is null) return false;
+        if (typeid(this) == typeid(b)) {
+            return roaring_bitmap_equals(this.roaring, (cast(Roaring)b).roaring);
+        }
+        return false;
+    }
+
+    bool opEquals(Roaring b) {
+        import std.stdio : writeln;
+        return roaring_bitmap_equals(this.roaring, b.roaring);
+    }
+
+    private roaring_bitmap_t* roaring;
+}
+
+unittest {
+    void test_construct(bool copyOnWrite) {
+        // create new empty bitmap
+        Roaring r1 = new Roaring;
+        r1.copyOnWrite = copyOnWrite;
+        // add values
+        foreach (i; 100 .. 1000) {
+            r1.add(i);
+        }
+        // check whether a value is contained
+        assert(r1.contains(500));
+        // check the number of bits
+        assert(r1.cardinality == 900);
+        //check optimization
+        auto size1 = r1.sizeInBytes;
+        r1.optimize;
+        assert(r1.sizeInBytes < size1);
+    }
+
+    void test_bitmapOf() {
+        const r = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        assert(r.contains(5));
+        assert(r.cardinality == 5);
+    }
+
+    void test_select() {
+        const r = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        uint el;
+        assert(r.select(3, el));
+        assert(el == 5);
+    }
+
+    void test_minimum_maximum() {
+        const r = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        assert(r.minimum == 1);
+        assert(r.maximum == 6);
+
+        const r2 = Roaring.bitmapOf(100, 0, uint.max);
+        assert(r2.minimum == 0);
+        assert(r2.maximum == uint.max);
+    }
+
+    void test_rank() {
+        const r = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        assert(r.rank(4) == 3);
+    }
+
+    void test_toArray() {
+        const r = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        uint[] a = r.toArray();
+        assert(a.length == 5);
+    }
+
+    void test_equals() {
+        const r1 = Roaring.fromRange(10, 20, 3);
+        const r2 = Roaring.fromArray([10, 13, 16, 19]);
+        assert(r1 == r2);
+
+        const r3 = Roaring.fromArray([10]);
+        assert(r1 != r3);
+    }
+
+    void test_union() {
+        const r1 = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        const r2 = Roaring.bitmapOf(6, 7, 8);
+        const r3 = r1 | r2;
+        assert(r3 == Roaring.bitmapOf(1, 2, 3, 5, 6, 7, 8));
+    }
+
+    void test_intersect() {
+        const r1 = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        const r2 = Roaring.bitmapOf(6, 7, 8);
+        const r3 = r1 & r2;
+        assert(r3 == Roaring.bitmapOf(6));
+    }
+
+    void test_write_read() {
+        const r1 = Roaring.bitmapOf(5, 1, 2, 3, 5, 6);
+        auto buf = r1.write();
+        const r2 = Roaring.read(buf);
+        assert(r1 == r2);
+    }
+    
+    test_bitmapOf();
+    test_construct(false);
+    test_construct(true);
+    test_equals();
+    test_intersect();
+    test_minimum_maximum();
+    test_rank();
+    test_select();
+    test_toArray();
+    test_union();
+    test_write_read();
+}
